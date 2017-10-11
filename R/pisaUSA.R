929d8e0f67c0b43c4b37dbdd2edf4e634bbf54f0
Joshua Rosenberg <jrosen@msu.edu>
2017-10-11 08:56:08 -0400

    add initial commit of pisaUSA

---
+++ R/pisaUSA.R
@@ -0,0 +1,15 @@
+#' All student questionnaire data from the 2015 PISA for students in the United States
+#'
+#' @source http://www.oecd.org/pisa/data/
+#' @format Data frame with columns
+#' @importFrom tibble tibble
+
+"pisaUSA"
+
+#' Select items from student questionnaire data from the 2015 PISA for students in the United States
+#'
+#' @source http://www.oecd.org/pisa/data/
+#' @format Data frame with columns
+#' @importFrom tibble tibble
+
+"pisaUSA_ss"
---
+++ R/process-data.R
@@ -0,0 +1,13 @@
+# process-data.R
+
+# library(readr)
+# library(dplyr)
+#
+# pisaUSA <- read_rds("R/rds_files/pisaUSA_ss.rds")
+# pisaUSA <- as_tibble(pisaUSA)
+# devtools::use_data(pisaUSA, overwrite = TRUE)
+#
+# pisaUSA_ss <- read_rds("R/rds_files/pisaUSA.rds")
+# pisaUSA_ss <- as_tibble(pisaUSA_ss)
+# devtools::use_data(pisaUSA_ss, overwrite = TRUE)
+
