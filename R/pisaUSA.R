792645ae00b65eaee545e9ab1c5f5d49dfb750c1
Joshua Rosenberg <jrosen@msu.edu>
2017-10-11 14:48:44 -0400

    add initial files

--- R/pisaUSA.R
+++ R/pisaUSA.R
@@ -2,14 +2,49 @@
 #'
 #' @source http://www.oecd.org/pisa/data/
 #' @format Data frame with columns
+#' #' \describe{
+#'   \item{CNTSTUID}{international student ID}
+#'   \item{SCHID}{international school ID}
+#'   ...
+#' }
 #' @importFrom tibble tibble
 
-"pisaUSA"
+"stu_quest"
 
-#' Select items from student questionnaire data from the 2015 PISA for students in the United States
+#' All student assessment data from the 2015 PISA for students in the United States
 #'
 #' @source http://www.oecd.org/pisa/data/
 #' @format Data frame with columns
+#' #' \describe{
+#'   \item{CNTSTUID}{international student ID}
+#'   \item{SCHID}{international school ID}
+#'   ...
+#' }
 #' @importFrom tibble tibble
 
-"pisaUSA_ss"
+"stu_assess"
+
+#' School-level items for responses in the USA
+#'
+#' @source http://www.oecd.org/pisa/data/
+#' @format Data frame with columns
+#' #' \describe{
+#'   \item{SCHID}{international school ID}
+#'   ...
+#' }
+#' @importFrom tibble tibble
+
+"sch_quest"
+
+#' Items from the PISA 2015 teacher questionnaire for responses in the USA
+#'
+#' @source http://www.oecd.org/pisa/data/
+#' @format Data frame with columns
+#' #' \describe{
+#'   \item{SCHID}{international school ID}
+#'   \item{CNTTCHID}{international teacher ID}
+#'   ...
+#' }
+#' @importFrom tibble tibble
+
+"tch_quest"
--- R/process-data.R
+++ R/process-data.R
@@ -1,13 +1,51 @@
-# process-data.R
-
+# # process-data.R
+#
 # library(readr)
 # library(dplyr)
+# library(tidyr)
+# library(stringr)
 #
-# pisaUSA <- read_rds("R/rds_files/pisaUSA_ss.rds")
-# pisaUSA <- as_tibble(pisaUSA)
-# devtools::use_data(pisaUSA, overwrite = TRUE)
+# stu_quest <- read_rds("raw_data/stu_quest.rds")
+# stu_quest <- as_tibble(stu_quest)
+# stu_quest <- mutate(stu_quest,
+#                     SCHID = str_sub(CNTSCHID, start = 4))
+# devtools::use_data(stu_quest, overwrite = TRUE)
 #
-# pisaUSA_ss <- read_rds("R/rds_files/pisaUSA.rds")
-# pisaUSA_ss <- as_tibble(pisaUSA_ss)
-# devtools::use_data(pisaUSA_ss, overwrite = TRUE)
-
+# stu_assess <- read_rds("raw_data/stu_assess.rds")
+# stu_assess <- as_tibble(stu_assess)
+# stu_assess <- mutate(stu_assess,
+#                      SCHID = str_sub(CNTSCHID, start = 4))
+# devtools::use_data(stu_assess, overwrite = TRUE)
+#
+# sch_quest <- read_rds("raw_data/sch_quest.rds")
+# sch_quest <- as_tibble(sch_quest)
+# sch_quest <- mutate(sch_quest,
+#                     SCHID = str_sub(CNTSCHID, start = 4))
+# sch_quest <- select(sch_quest, CNT, CNTRYID, SCHID, CNTSCHID, everything())
+# devtools::use_data(sch_quest, overwrite = TRUE)
+#
+# tch_quest <- read_rds("raw_data/tch_quest.rds")
+# tch_quest <- as_tibble(tch_quest)
+# tch_quest <- mutate(tch_quest,
+#                     SCHID = str_sub(CNTSCHID, start = 4))
+# devtools::use_data(tch_quest, overwrite = TRUE)
+#
+# escs_2012 <- read_rds("raw_data/escs_2012.rds")
+# escs_2012 <- as_tibble(escs_2012)
+# escs_2012 <- rename(escs_2012, CNT = cnt, SCHID = schoolid)
+# escs_2012 <- mutate(escs_2012,
+#                     SCHID = str_sub(SCHID, start = 3))
+# devtools::use_data(escs_2012, overwrite = TRUE)
+#
+# # joining stu assessment and stu questionnaire
+# x <- left_join(stu_assess, stu_quest, by = "CNTSTUID")
+#
+# # joining teacher or school data with ESCS data
+# x <- left_join(tch_quest, escs_2012, by = "SCHID")
+#
+# # joining stu data with teacher or school data
+# x <- left_join(stu_quest, tch_quest, by = "SCHID")
+# x <- left_join(stu_quest, escs_2012, by = "SCHID")
+#
+# x <- left_join(stu_assess, tch_quest, by = "SCHID")
+# x <- left_join(stu_assess, escs_2012, by = "SCHID")
